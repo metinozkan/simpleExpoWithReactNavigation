93b20d6687963b10eb2ab36eb12ac996b52fc417
metinozkn <metin.ozkn@hotmail.com>
2020-10-31 18:10:51 +0300

    started project added react navigation

--- main/index.js
+++ main/index.js
@@ -6,7 +6,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Merhaba</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
